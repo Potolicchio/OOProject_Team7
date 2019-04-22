4772a5d29ae9a6b6cfead870df987b7be27bc4a6
DamoyHyman <hyman_damoy@yahoo.com>
2019-04-21 22:18:28 -0400

    Login up and running and working on registration form.

--- OOProject/src/gui/Login.java
+++ OOProject/src/gui/Login.java
@@ -4,15 +4,26 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
-import javax.swing.UIManager;
+import java.awt.Cursor;
 
-public class Login {
+import javax.swing.UIManager;
+import javax.swing.JSeparator;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+import java.awt.Canvas;
+import java.awt.SystemColor;
+
+public class Login 
+{
 
 	private JFrame frmLogin;
 	private JTextField txtUsername;
@@ -21,13 +32,18 @@ public class Login {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
+	public static void main(String[] args) 
+	{
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				try 
+				{
 					Login window = new Login();
 					window.frmLogin.setVisible(true);
-				} catch (Exception e) {
+				} catch (Exception e) 
+				{
 					e.printStackTrace();
 				}
 			}
@@ -37,67 +53,138 @@ public class Login {
 	/**
 	 * Create the application.
 	 */
-	public Login() {
+	public Login() 
+	{
 		initialize();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void initialize() 
+	{
 		frmLogin = new JFrame();
+		frmLogin.setResizable(false);
 		frmLogin.getContentPane().setForeground(new Color(0, 0, 204));
-		frmLogin.getContentPane().setBackground(Color.GRAY);
+		frmLogin.getContentPane().setBackground(SystemColor.windowBorder);
 		frmLogin.setBackground(Color.GRAY);
 		frmLogin.setForeground(Color.GRAY);
 		frmLogin.setTitle("Login");
-		frmLogin.setBounds(100, 100, 450, 300);
+		frmLogin.setBounds(200, 200, 448, 295);
 		frmLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmLogin.getContentPane().setLayout(null);
 		
+		/**
+		 * Create the labeling
+		 */
 		JLabel lblUsernameLabel = new JLabel("Username");
-		lblUsernameLabel.setFont(UIManager.getFont("FileChooser.listFont"));
-		lblUsernameLabel.setBounds(24, 66, 75, 14);
+		lblUsernameLabel.setFont(new Font("Arial", Font.ITALIC, 14));
+		lblUsernameLabel.setBounds(24, 99, 75, 17);
 		frmLogin.getContentPane().add(lblUsernameLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("Password");
-		lblNewLabel_1.setBounds(24, 135, 75, 14);
-		frmLogin.getContentPane().add(lblNewLabel_1);
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setFont(new Font("Arial", Font.ITALIC, 14));
+		lblPassword.setBounds(24, 154, 75, 14);
+		frmLogin.getContentPane().add(lblPassword);
 		
 		txtUsername = new JTextField();
-		txtUsername.setBounds(147, 63, 173, 20);
+		txtUsername.setToolTipText("Enter Username here");
+		txtUsername.setBounds(147, 98, 207, 20);
 		frmLogin.getContentPane().add(txtUsername);
 		txtUsername.setColumns(10);
 		
 		txtPassword = new JPasswordField();
-		txtPassword.setBounds(147, 132, 173, 20);
+		txtPassword.setToolTipText("Enter Password");
+		txtPassword.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 20));
+		txtPassword.setBounds(147, 146, 207, 20);
 		frmLogin.getContentPane().add(txtPassword);
 		
+		/**
+		 * Creating the buttons + their functionality
+		 */
 		JButton btnLogin = new JButton("Login");
-		btnLogin.setBounds(24, 227, 89, 23);
+		btnLogin.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnLogin.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				String password = txtPassword.getText();
+				String username = txtUsername.getText();
+				
+				if (password.contains("king") && username.contains("One")) {
+					txtPassword.setText(null);
+					txtUsername.setText(null);
+					
+					frmLogin.setVisible(false);
+					Home.main(null);
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(null, "Invalid Login Details", "Login Error", JOptionPane.ERROR_MESSAGE);
+				}
+				
+			}
+		});
+		btnLogin.setBounds(49, 227, 89, 23);
 		frmLogin.getContentPane().add(btnLogin);
 		
 		JButton btnReset = new JButton("Reset");
-		btnReset.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+		btnReset.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnReset.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{
 				txtUsername.setText(null);
 				txtPassword.setText(null);
 			}
 		});
-		btnReset.setBounds(169, 227, 89, 23);
+		btnReset.setBounds(183, 227, 89, 23);
 		frmLogin.getContentPane().add(btnReset);
 		
 		JButton btnExit = new JButton("Exit");
-		btnExit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			 System.exit(0);
+		btnExit.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnExit.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				frmLogin = new JFrame("Exit");
+				if (JOptionPane.showConfirmDialog(frmLogin, "Confirm if you want to exit", "Login System",
+					 JOptionPane.YES_NO_OPTION)== JOptionPane.YES_NO_OPTION)
+				{
+					System.exit(0);
+				}
 			}
 		});
-		btnExit.setBounds(319, 227, 89, 23);
+		
+		btnExit.setBounds(308, 227, 89, 23);
 		frmLogin.getContentPane().add(btnExit);
 		
 		JLabel lblLogin = new JLabel("Login");
-		lblLogin.setBounds(212, 22, 46, 14);
+		lblLogin.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD | Font.ITALIC, 22));
+		lblLogin.setBounds(183, 22, 75, 28);
 		frmLogin.getContentPane().add(lblLogin);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBackground(SystemColor.desktop);
+		separator.setBounds(10, 204, 422, 2);
+		frmLogin.getContentPane().add(separator);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBackground(SystemColor.desktop);
+		separator_1.setBounds(10, 79, 412, 8);
+		frmLogin.getContentPane().add(separator_1);
+		
+		JLabel lblNew_user = new JLabel("New User?, Register");
+		lblNew_user.setFont(UIManager.getFont("ToolTip.font"));
+		lblNew_user.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		lblNew_user.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				Registration_Form.main(null);
+			}
+		});
+		
+		
+		lblNew_user.setBounds(297, 179, 135, 14);
+		frmLogin.getContentPane().add(lblNew_user);
 	}
 }
