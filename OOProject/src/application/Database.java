73c770aa7d52bb2872f88f5597ffac892e55659f
Potolicchio <miguelpotolicchio@gmail.com>
2019-04-22 19:52:51 -0400

    Commit update to Database file

--- OOProject/src/application/Database.java
+++ OOProject/src/application/Database.java
@@ -3,35 +3,38 @@ package application;
 import java.sql.*;
 
 public class Database {
-
-	public static void main(String[] args) 
+	
+	public static Connection getConnection()
 	{
-		// TODO Auto-generated method stub
+		Connection myConn = null;
 		
 		try 
-		{
+		{	
+			//No longer needed in newer versions of the J Connector
 			//Class.forName("com.mysql.cj.jdbc.Driver");
-			
+				
 			// 1. Get a connection to the database
-			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ooproject", "root", "Poto@SQL2019!");
-			
-			// 2. Create a statement
-			Statement myStmt = myConn.createStatement();
-			
-			// 3. Execute SQL query
-			ResultSet myRs = myStmt.executeQuery("select * from People");
-			
-			// 4. Process the result set
-			while (myRs.next())
-			{
-				System.out.println(myRs.getString("lastName") + ", " + myRs.getString("firstName"));
-			}
+			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ooproject", "root", "Poto@SQL2019!");
 		}
-		catch (Exception exc)
+		catch (Exception exc)	
 		{
-			exc.printStackTrace();
+			exc.printStackTrace();	
 		}
-
+		
+		return myConn;
 	}
 
 }
+
+//2. Create a statement			
+//Statement myStmt = myConn.createStatement();
+			
+// 3. Execute SQL query		
+//ResultSet myRs = myStmt.executeQuery("select * from People");
+				
+			
+// 4. Process the result set
+//while (myRs.next())		
+//{			
+	//System.out.println(myRs.getString("lastName") + ", " + myRs.getString("firstName"));			
+//}
